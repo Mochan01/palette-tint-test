b7245053532ead792f473f3243d739717a705bda
Mochan01 <ryohei.s@fantamstick.jp>
2021-04-13 13:55:34 +0900

    Duplication test.

--- src/index.js
+++ src/index.js
@@ -7,17 +7,28 @@ class MyGame extends Phaser.Scene {
   }
 
   preload() {
+    // Logo 1.
     this.load.image("logo", logoImg);
+
+    // Logo 2.
+    this.load.image("logo2", logoImg);
   }
 
   create() {
-    this.cameras.main.setBackgroundColor("#dddddd")
-    const logo = this.add.image(400, 150, "logo");
+    this.cameras.main.setBackgroundColor("#dddddd");
 
-    // Setting tint.
-    logo.setInteractive();
+    // Logo 1.
+    const logo = this.add.image(400, 150, "logo");
+    logo.setInteractive(this.input.makePixelPerfect());
     logo.on('pointerup', () => {
       logo.setTint(0xff0000)
+    });
+
+    // Logo 2.
+    const logo2 = this.add.image(400, 450, "logo2");
+    logo2.setInteractive(this.input.makePixelPerfect());
+    logo2.on('pointerup', () => {
+      logo2.setTint(0x9900FF)
     });
   }
 }
